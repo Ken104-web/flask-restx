a8c69739f7db849f966332888ac6a17fa6b00cad
KenCorp <kennethwarui35@gmail.com>
2023-11-20 08:58:46 +0300

    initatialized api

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .extensions import api, db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+
+    api.init_app(app)
+    db.init_app(app)
+
+    return app
